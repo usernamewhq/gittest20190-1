1cffeebb75b4640b67a62e052e4d5a4e21c962ae
wanghongqiang <1964685145@qq.com>
2019-08-22 13:50:23 +0800

    修改了get方法

--- gitmaster/src/main/java/com/jsnb/GameContrlloer.java
+++ gitmaster/src/main/java/com/jsnb/GameContrlloer.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GameContrlloer {
     public String get(){
+        String a= "1111111";
         return "8888";
     }
 }
