c8d7363378861d76a55786f565461ee86f1d6a81
wanghongqiang <1964685145@qq.com>
2019-08-22 14:47:16 +0800

    修改了user方法2

--- gitmaster/src/main/java/com/jsnb/User.java
+++ gitmaster/src/main/java/com/jsnb/User.java
@@ -1,9 +1,9 @@
 package com.jsnb;
 
 public class User {
-    String a="2";
-    String b="3";
-    int aaa=5;
-    int aaac = 6;
-    boolean falg = false;
+    String a="1";
+    String b="2";
+    int aaa=3;
+    int aaac = 4;
+    boolean falg = true;
 }
